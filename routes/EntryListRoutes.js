0101ca4a6c42757e868f7bc4315c3b332fd74d22
ReaganG <rbgrunwald1997@gmail.com>
2021-05-04 11:53:30 -0500

    grid update

--- routes/EntryListRoutes.js
+++ routes/EntryListRoutes.js
@@ -4,11 +4,21 @@ const router = require("express").Router();
 
 router.post('/api/createEntry', async (req, res) => {
     const created = await EntryList.create(req.body);
+    let currentSeason = await Season.find({});
+    currentSeason = currentSeason[0];
+
+    const newWeekend = await Weekend.create({});
+    const updatedSeason = await Season.findOneAndUpdate({_id: currentSeason._id}, {$push: {grid: created._id}});
     res.send(created);
 })
 
 router.get('/api/currentGrid', async (req, res) => {
     const grid = await EntryList.find({});
+    let currentSeason = await Season.find({});
+    currentSeason = currentSeason[0];
+
+    const newWeekend = await Weekend.create({});
+    const updatedSeason = await Season.findOneAndUpdate({_id: currentSeason._id}).populate('grid');
     res.send(grid);
 })
 
