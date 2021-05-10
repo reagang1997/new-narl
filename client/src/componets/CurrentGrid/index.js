ff22b906949f311f72f59e38b35e613c427a28a2
ReaganG <rbgrunwald1997@gmail.com>
2021-05-09 20:06:11 -0500

    race info style

--- client/src/componets/CurrentGrid/index.js
+++ client/src/componets/CurrentGrid/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import TeamIcon from '../TeamIcon';
-import {Card} from 'react-bootstrap';
+import { Card, Col, Row } from 'react-bootstrap';
 
 const CurrentGrid = () => {
 
@@ -18,21 +18,33 @@ const CurrentGrid = () => {
         setDriverCount(currentGrid.data.length);
     }
 
-    return(
+    return (
         <div className='f1'>
-            <Card body className='box  week-card' style={{marginTop: '50px', marginBottom: '50px'}}>
+            <Card body className='box  week-card' style={{ marginTop: '50px', marginBottom: '50px' }}>
                 <h1 className='this-week'>Grid</h1>
                 <p>Drivers: {driverCount}</p>
                 {grid.map(driver => {
                     return (
-                        <Card body className='shadow' style={{width: '95%', margin: 'auto', marginTop: '5px'}}>
-                            <TeamIcon teamName={driver.team}></TeamIcon>
-                            <span>#{driver.driverNumber}   {driver.name}</span>
+                        <Card body className='shadow' style={{ width: '95%', margin: 'auto', marginTop: '5px' }}>
+                            <Row>
+                                <Col md={3}>
+                                    <div  style={{marginLeft: '35px'}}>
+                                        
+                                    <TeamIcon teamName={driver.team}></TeamIcon>
+                                    </div>
+
+                                </Col>
+                                <Col md={9}>
+                                    <div style={{marginTop: '15px'}}>
+                                    <span >#{driver.driverNumber}   {driver.name}</span>
+                                    </div>
+                                </Col>
+                            </Row>
                         </Card>
                     )
                 })}
 
-                
+
             </Card>
 
         </div>
