85be073437d1634dd87efe7d7f39541566087827
ReaganG <rbgrunwald1997@gmail.com>
2021-05-24 19:55:01 -0500

    pts on driver home

--- client/src/componets/pages/DriverHome.js
+++ client/src/componets/pages/DriverHome.js
@@ -12,6 +12,7 @@ import SeasonStats from '../StatsComponents/SeasonStats';
 import CareerStats from '../StatsComponents/CareerStats';
 import DriverHomeNav from '../DriverHomeNav';
 import EditProfile from '../EditProfile';
+import { set } from 'mongoose';
 
 const DriverHome = ({ loggedIn, driver, setDriver, guid, setGuid }) => {
     const [rsvp, setRsvp] = useState('');
@@ -19,7 +20,12 @@ const DriverHome = ({ loggedIn, driver, setDriver, guid, setGuid }) => {
     const [number, setNumber] = useState('')
     const [nav, setNav] = useState('Stats');
 
+    const [wdc, setWdc] = useState([]);
+    const [pos, setPos] = useState(0);
 
+    useEffect(() => {
+        getWDC();
+    }, []);
 
 
     useEffect(() => {
@@ -29,6 +35,16 @@ const DriverHome = ({ loggedIn, driver, setDriver, guid, setGuid }) => {
     useEffect(() => {
 
     }, [driver]);
+
+    const getWDC = async () => {
+        const tmpWDC = await axios.get('/api/WDC');
+        setWdc(tmpWDC.data);
+        tmpWDC.data.forEach((tmpDriver, i) => {
+            if (tmpDriver.name === driver.name) {
+                setPos(i + 1);
+            }
+        })
+    }
 
     const getRsvp = () => {
         setRsvp(driver.rsvp);
@@ -75,19 +91,55 @@ const DriverHome = ({ loggedIn, driver, setDriver, guid, setGuid }) => {
 
             { driver ?
                 <div className='f1' style={{ width: '1350px', margin: 'auto', marginTop: '25px' }} >
+                    <Row>
+                        <Col md={10}>
+                            <h1 id='white' style={{ fontSize: '55px' }}>{driver.name}</h1>
+                        </Col>
+                        <Col md={2}>
+                            <div className='pts-card'>
+                                <Row>
+                                    <Col md={12}>
+                                        <div style={{ width: 'fit-content', margin: 'auto', marginBottom: '-15px' }}>
+                                            <h3>Standing</h3>
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col md={12}>
+                                        <div style={{ marginLeft: '32px' }}>
+                                            <span className='pos'>{pos}</span>
+                                            {pos === 1 || pos === 21 ? <span style={{ margin: '0' }}>st</span> : <div style={{ display: 'none' }} />}
+                                            {pos === 2 ? <span style={{ margin: '0' }}>nd</span> : <div style={{ display: 'none' }} />}
+                                            {pos === 3 || pos === 23 ? <span style={{ margin: '0' }}>rd</span> : <div style={{ display: 'none' }} />}
+                                            {pos >= 4 && pos <= 20 ? <span style={{ margin: '0' }}>th</span> : <div style={{ display: 'none' }} />}
+                                            {pos > 23 ? <span style={{ margin: '0' }}>th</span> : <div style={{ display: 'none' }} />}
 
-                    <h1 id='white' style={{ fontSize: '55px' }}>{driver.name}</h1>
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col md={12}>
+                                        <div style={{ marginLeft: '35px' }}>
+                                            <span className='pts'>{driver.points}</span>
+                                            <span style={{ margin: '0px' }}>PTS</span>
+                                        </div>
+                                    </Col>
+                                </Row>
+
+                            </div>
+                        </Col>
+                    </Row>
                     <h3 id='white'>{driver.team}</h3>
                     <h8 id='white'>Server Password: narlseason2</h8>
                     {/* <h4><TeamIcon teamName={driver.team} /><span>{driver.team}</span></h4> */}
                     <div style={{ width: 'fit-content', margin: 'auto', marginTop: '50px' }}>
                         <Row style={{ width: '1500px', marginLeft: '-200px' }}>
                             <Col lg={2}>
-                                <DriverHomeNav  nav={nav} setNav={setNav} />
+                                <DriverHomeNav nav={nav} setNav={setNav} />
                             </Col>
                             {nav === 'Stats' ?
-                                <div style={{marginLeft: '120px', }}>
-                                    <Row style={{width: 'fit-content', margin: 'auto'}}>
+                                <div style={{ marginLeft: '120px', }}>
+                                    <Row style={{ width: 'fit-content', margin: 'auto' }}>
                                         <Col lg={6}>
                                             <SeasonStats wins={driver.wins} pts={driver.points} fl={driver.fastestLaps}></SeasonStats>
                                         </Col>
@@ -98,7 +150,7 @@ const DriverHome = ({ loggedIn, driver, setDriver, guid, setGuid }) => {
                                 </div>
                                 : <div></div>}
                             {nav === 'Registration' ?
-                                <div style={{width: 'fit-content', margin: 'auto'}}>
+                                <div style={{ width: 'fit-content', margin: 'auto' }}>
                                     {
                                         driver.team !== 'Reserve' ? <RSVP setRsvp={setRsvp} updateRsvp={updateRsvp} driver={driver} rsvp={rsvp} /> :
                                             <div>
@@ -109,9 +161,9 @@ const DriverHome = ({ loggedIn, driver, setDriver, guid, setGuid }) => {
                                 </div> : <div></div>
                             }
                             {nav === 'Edit Profile' ?
-                                <div style={{width: 'fit-content', margin: 'auto', marginTop: '0px'}}>
+                                <div style={{ width: 'fit-content', margin: 'auto', marginTop: '0px' }}>
                                     {
-                                        <EditProfile driver={driver} setDriver={setDriver}/>
+                                        <EditProfile driver={driver} setDriver={setDriver} />
                                     }
                                 </div> : <div></div>
                             }
